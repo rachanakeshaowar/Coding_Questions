1166384f6e680d5216f09ffda24a43ec546c621e
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-13 19:41:55 +0530

    print subsequence

---
+++ Recursion2/PrintSSQProgram6Training.java
@@ -0,0 +1,18 @@
+public class PrintSSQProgram6Training {
+    public static void main(String[] args) {
+        int[] arr ={1,2,3};
+        printSubSeq(arr,0,"");
+        System.out.println("no. of subsequences ces are:" + count);
+    }
+    public static int count=0;
+    public static void printSubSeq(int[] arr, int startIndex ,String ans ){
+        if(startIndex==arr.length){
+            // Print the curr ans
+            System.out.println(ans);
+            count ++ ;
+            return ;
+        }
+        printSubSeq(arr,startIndex +1,ans); //not take
+        printSubSeq(arr,startIndex+1,ans + arr[startIndex]); //take
+    }
+}
