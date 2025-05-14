eeab657718f2238c9c85f6b90463006efff10d15
rachanakeshaowar <keshrachana0@gmail.com>
2025-05-14 17:35:57 +0530

    Move stack in same order

---
+++ Stack/MoveStcakInSameOrder.java
@@ -0,0 +1,38 @@
+import java.util.Stack;
+import java.util.Scanner;
+public class MoveStcakInSameOrder {
+
+    public static void main(String[] args) {
+       Scanner sc =new Scanner(System.in);
+        Stack<Integer> st=new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        System.out.println(st);
+        Stack<Integer> gt =new Stack<>();
+        while(st.size()>0){
+//            int x =st.peek();
+//            rt.push(x);
+//            st.pop();
+            gt.push(st.pop());
+        }
+        Stack<Integer> rt =new Stack<>();
+        while(gt.size()>0){
+            rt.push(gt.pop());
+        }
+        System.out.println(rt);
+        // here same time complexity and space complexity
+//        int n;
+//        System.out.println("Enter the elements you want to insert:");
+//        n = sc.nextInt();
+//        System.out.println("enter the elements ");
+//        for(int i=0;i<=n;i++){
+//            int x =sc.nextInt();
+//            st.push(x);
+//
+////        }
+//        System.out.println(st);
+    }
+}
