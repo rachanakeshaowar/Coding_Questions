c5db1b65681464bcd827ecc5c47b3b2425364476
rachanakeshaowar <keshrachana0@gmail.com>
2025-06-05 23:36:41 +0530

    HashSet Basics

---
+++ HashMap/HashSetBasics.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+public class HashSetBasics {
+    public static void main(String[] args) {
+        HashSet<String> st=new HashSet<>();
+        st.add("James");
+        st.add("scott");
+        st.add("James");
+        System.out.println(st);
+        System.out.println(st.contains("James"));
+        System.out.println(st.size());
+        System.out.println(st);
+        st.remove("James");
+        System.out.println(st.contains("James"));
+        System.out.println(st.size());
+        st.add("sott");
+        for(String s: st){
+            System.out.print(s+" ");
+        }
+        System.out.println(st);
+
+    }
+}
